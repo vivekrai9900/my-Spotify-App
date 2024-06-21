ea29287beb781f99ddd28dff12596c1c1bc125ac
Vivek Rai <122623874+vivekrai9900@users.noreply.github.com>
2024-06-22 02:15:23 +0530

    all logic Part done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState }  from 'react'
+
+import {setupPlayer, addTrack } from '../musicPlayerService'
 
 export default function App() {
+
+  const [isPlayerReady, setIsPlayerReady] = useState(false)
+
+  async function setup(){
+    let isSetUp = await setupPlayer()
+
+    if(isSetUp){
+      await addTrack();
+    }
+
+    setIsPlayerReady(isSetUp)
+  }
+
+
+  useEffect(() => {
+   setup() 
+  }, [])
+
+  if(!isPlayerReady){
+    return(
+      <SafeAreaView>
+        <ActivityIndicator/>
+      </SafeAreaView>
+    )
+  }
+  
   return (
     <View>
       <Text>App</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  }
+})
