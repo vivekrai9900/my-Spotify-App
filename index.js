a49c2be2c503d3b1887d7437780ebeec66f88470
Vivek Rai <122623874+vivekrai9900@users.noreply.github.com>
2024-06-22 01:59:05 +0530

    play services are created

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 /**
  * @format
  */
+import TrackPlayer from 'react-native-track-player';
 
+import { playbackService } from './musicPlayerService';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+TrackPlayer.registerPlaybackService(()=> playbackService);
